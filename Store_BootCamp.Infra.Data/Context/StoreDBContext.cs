aef5dffe95806192d9951e9232c7165ca2a42670
Masoud Kian <masoudkiannezhad@gmail.com>
2024-01-01 18:20:01 +0330

    Set DbContext -- Start Initial Theme

---
+++ Store_BootCamp.Infra.Data/Context/StoreDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store_BootCamp.Domain.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_BootCamp.Infra.Data.Context
+{
+    public class StoreDBContext : DbContext
+    {
+        public StoreDBContext(DbContextOptions<StoreDBContext> options) : base (options)
+        {
+                
+        }
+
+        #region Dbset
+
+        public DbSet<User> Users { get; set; }
+
+        #endregion
+    }
+}
