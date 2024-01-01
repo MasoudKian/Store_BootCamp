4b8d93a4aeb40d24506a8d4f3575d51804f163a3
Masoud Kian <masoudkiannezhad@gmail.com>
2024-01-01 18:10:44 +0330

    Create User & BaseEntity Model

---
+++ Store_BootCamp.Domain/Models/Account/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_BootCamp.Domain.Models.Account
+{
+    public class User
+    {
+        [Display(Name = "نام و نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(400, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Fullname { get; set; }
+
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(400, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string UserName { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(400, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [EmailAddress(ErrorMessage ="ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(400, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Password { get; set; }
+
+        [Display(Name = "کد فعال سازی")]
+        public string ActiveEmailCode { get; set; }
+
+        [Display(Name = "فعال / غیرفعال")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "ادمین است / ادمین نیست")]
+        public bool IsAdmin { get; set; }
+
+    }
+}
---
+++ Store_BootCamp.Domain/Models/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_BootCamp.Domain.Models
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("تاریخ ثبت نام")]
+        public DateTime CreateDate { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
