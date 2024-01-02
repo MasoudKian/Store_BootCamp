a21c0922cac8ff3c820067141197ac4ed49743b5
Masoud Kian <masoudkiannezhad@gmail.com>
2024-01-02 06:40:57 +0330

    SiteViewComponent - Header - Footer

---
+++ Store_BootCamp.Web/ViewComponents/SiteViewComponents.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store_BootCamp.Web.ViewComponents
+{
+	#region SiteHeader 
+
+	public class SiteHeader : ViewComponent
+	{
+		public SiteHeader() { }
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+            return await Task.FromResult((IViewComponentResult)View("SiteHeader"));
+        }
+	}
+
+    #endregion
+
+    #region SiteFooter 
+
+    public class SiteFooter : ViewComponent
+    {
+        public SiteFooter() { }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("SiteFooter"));
+        }
+    }
+
+    #endregion
+}
