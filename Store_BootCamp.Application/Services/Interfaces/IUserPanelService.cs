ac762a19fa9dc9f97f8f636a9d592b377bd57522
Masoud Kian <masoudkiannezhad@gmail.com>
2024-01-03 01:48:26 +0330

    Start Profile InformationUser In UserPanel

--- Store_BootCamp.Application/Services/Interfaces/IUserPanelService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Store_BootCamp.Application.ViewModels.Account;
-
-namespace Store_BootCamp.Application.Services.Interfaces
-{
-    public interface IUserPanelService
-    {
-        ProfileInformationUserViewModel GetInformationUser(string email);
-    }
-}
