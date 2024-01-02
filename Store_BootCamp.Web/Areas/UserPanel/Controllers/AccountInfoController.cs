8a2be1cdde8835065ef83533f4f66e1e1482ae06
Masoud Kian <masoudkiannezhad@gmail.com>
2024-01-03 00:30:40 +0330

    Account Info Controller

---
+++ Store_BootCamp.Web/Areas/UserPanel/Controllers/AccountInfoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store_BootCamp.Web.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    public class AccountInfoController : Controller
+    {
+        [Route("Profile")]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
