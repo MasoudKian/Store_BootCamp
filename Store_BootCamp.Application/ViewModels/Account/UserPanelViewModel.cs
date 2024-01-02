93138a2b467db2f1dfd36ee4b7aa9ed77db25f89
Masoud Kian <masoudkiannezhad@gmail.com>
2024-01-03 00:50:21 +0330

    UserPanel ViewModel

---
+++ Store_BootCamp.Application/ViewModels/Account/UserPanelViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store_BootCamp.Application.ViewModels.Account
+{
+    public class ProfileInformationUserViewModel
+    {
+
+        public string Fullname { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserImage { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+    }
+}
