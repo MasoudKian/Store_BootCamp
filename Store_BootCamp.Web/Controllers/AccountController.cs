69bfb463da52306f9f6a1b2bc1e9b4081aac14e9
Masoud Kian <masoudkiannezhad@gmail.com>
2024-01-02 08:42:06 +0330

    Set Register View

--- Store_BootCamp.Web/Controllers/AccountController.cs
+++ Store_BootCamp.Web/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ namespace Store_BootCamp.Web.Controllers
 {
     public class AccountController : Controller
     {
+        [Route("Register")]
         public IActionResult Register()
         {
             return View();
