76b0c313ae1749519e8d7245655e09e80a773d10
Masoud Kian <masoudkiannezhad@gmail.com>
2024-01-03 07:53:29 +0330

    UserPanel Profie

--- Store_BootCamp.Web/Areas/UserPanel/Controllers/HomeController.cs
+++ Store_BootCamp.Web/Areas/UserPanel/Controllers/HomeController.cs
@@ -5,16 +5,10 @@ namespace Store_BootCamp.Web.Areas.UserPanel.Controllers
     [Area("UserPanel")]
     public class HomeController : Controller
     {
-        [Route("/UserPanel/Orders")]
+        [Route("Profile")]
         public IActionResult Index()
         {
             return View();
         }
-
-        //[Route("/UserPanel/FavoritesList")]
-        //public IActionResult FavoritesList()
-        //{
-        //    return View();
-        //}
     }
 }
--- Store_BootCamp.Web/Controllers/AccountController.cs
+++ Store_BootCamp.Web/Controllers/AccountController.cs
@@ -108,7 +108,7 @@ namespace Store_BootCamp.Web.Controllers
                     };
 
                     HttpContext.SignInAsync(pricipal, properties);
-                    return Redirect("/");
+                    return Redirect("/Profile");
                 }
                 else
                 {
